5055ba83737bfdb3f39b776f0e51106dd688f6af
Shuo Wang Hsu <shuohsu@google.com>
2025-09-09 18:58:19 +0000

    feat(build): Add Bazel build and test workflow
    This commit introduces Bazel as a complete build and test option for both local development and CI.
    
    A new `BazelTask` builds and tests all targets (`:all`). Downstream tasks for packaging and testing are now aware of the `bazel-bin` output directory, enabling a full end-to-end workflow (e.g., `--task localrunall --task bazel`).
    
    Also includes dep fixes for rootcanal and wpa_supplicant_8
    
    Change-Id: I20c8c220494a8f1a7ea07de46187e23490c71e07

---
+++ scripts/tasks/bazel_task.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+#
+# Copyright 2024 - The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the',  help="License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an',  help="AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import platform
+
+from tasks.task import Task
+from utils import AOSP_ROOT, run
+
+
+class BazelTask(Task):
+
+  def __init__(self, args, env):
+    super().__init__("Bazel")
+    self.env = env
+    system = f"{platform.system().lower()}-x86_64"
+    self.path = AOSP_ROOT / "prebuilts" / "bazel" / system / "bazel"
+
+  def do_run(self):
+    # Build
+    run(
+        [self.path, "build", ":all", "--config=release"],
+        self.env,
+        "bazel build",
+        AOSP_ROOT / "tools" / "netsim",
+    )
+
+    # Test
+    run(
+        [self.path, "test", ":all", "--config=release"],
+        self.env,
+        "bazel test",
+        AOSP_ROOT / "tools" / "netsim",
+    )
+
+    return True
