52e3468bba031fad5f01478e50506b5fd0657e87
Sameer Puri <sameerpuri@google.com>
2026-03-03 23:39:38 +0000

    Enable netsim-next on Windows
    All build/test issues for netsim next on Windows are resolved, safe to
    enable now.
    
    Fixes: 489522842
    Test: ./scripts/build_tools.py --task Compile RunTest
    Change-Id: Ifeb37eddffaefe4180256644f9b58b73abf2f1c9

--- scripts/tasks/compile_install_task.py
+++ scripts/tasks/compile_install_task.py
@@ -91,20 +91,12 @@ class CompileInstallTask(Task):
       dest_dir.mkdir(exist_ok=True, parents=True)
 
       # Copy netsim binaries
-      if platform.system() == "Windows":
-        # TODO: Netsim Next is not yet built on Windows.
-        # We don't copy netsim next (netsimx/netsimdx) for now on Windows.
-        binaries = {
-            "netsim": "netsim",
-            "netsimd": "netsimd",
-        }
-      else:
-        binaries = {
-            "netsim": "netsim",
-            "netsimd": "netsimd",
-            "netsimx": "next/cli/netsim",
-            "netsimdx": "next/daemon/daemon",
-        }
+      binaries = {
+          "netsim": "netsim",
+          "netsimd": "netsimd",
+          "netsimx": "next/cli/netsim",
+          "netsimdx": "next/daemon/daemon",
+      }
 
       for binary, src in binaries.items():
         if platform.system() == "Windows":
