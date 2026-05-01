feecec6f5dd4e0c28f3f3a9e91c55de0adb6ddc8
Sameer Puri <sameerpuri@google.com>
2026-05-01 22:55:28 +0000

    [netsim] Remove RunVerify from runtest mapping
    The runtest task alias was mapping to both Test and RunVerify.
    RunVerify is a heavy end-to-end test and should not be triggered by runtest.
    
    Test: ./scripts/build_tools.py --task runtest
    Bug: 508632031
    Change-Id: I9fe69c31f2b73698864b3f8851ba7bedca494114

--- scripts/tasks/__init__.py
+++ scripts/tasks/__init__.py
@@ -76,7 +76,7 @@ def get_tasks(args, env) -> Mapping[str, Task]:
       "compile": ["Build"],
       "compileinstall": ["CompileInstall"],
       "test": ["Test"],
-      "runtest": ["Test", "RunVerify"],
+      "runtest": ["Test"],
       "zipartifact": ["ZipArtifact"],
       "installemulator": ["InstallEmulator"],
       "runpytest": ["RunPyTest"],
