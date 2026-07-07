bd122aac925cefda119529550af5fa3f5a5868ff
Bailey Kuo <formosa@google.com>
2026-07-07 21:14:07 +0000

    Version Bump to 0.3.115 (next to 1.0.14)
    Change-Id: I32ab36c3cd3d1caf6af701f06a3c296c6fcf8a05

--- next/daemon/src/version.rs
+++ next/daemon/src/version.rs
@@ -5,7 +5,7 @@
 
 /// Version library.
 
-pub const VERSION: &str = "1.0.13";
+pub const VERSION: &str = "1.0.14";
 
 pub fn get_version() -> String {
     VERSION.to_owned()
--- rust/daemon/src/version.rs
+++ rust/daemon/src/version.rs
@@ -5,7 +5,7 @@
 
 /// Version library.
 
-pub const VERSION: &str = "0.3.114";
+pub const VERSION: &str = "0.3.115";
 
 pub fn get_version() -> String {
     VERSION.to_owned()
